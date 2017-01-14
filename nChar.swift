127cf4b2cd287c96844bc0c259e229f1d9e8dfa1
LunaWax <lunaisacoolguy@gmail.com>
2017-01-14 16:14:40 +0300

    add nChar 1.0
    Add the first version of the very first code bit of what’s soon going
    to be the best collection of Swift extensions snippets ever!

---
+++ nChar.swift
@@ -0,0 +1,41 @@
+//
+// Swift Justice: nChar function 1.0
+//
+// The ingenious nChar function is there for you when you need an easy way to get any string's substring based
+// on its index, as if you were dealing with an array (similar to how it works in JavaScript).
+// 
+// **User Manual** :
+//  1. Call the nChar function with two parameters:
+//      str: any string (or constant/variable with a String-type value) you like
+//      indx: the target index (we like to count from 0, i.e. 'S' in 'Swift' has the index of 0)
+//  2. Let the magic begin
+//  3. Receive the character you were looking for (!!!IMPORTANT!!) as a 'String', not 'Character'
+//     This means you can use it for your string-combining shady business straight away!
+//  4. ????
+//  5. Profit
+//
+//
+//          (sound of trumpets)
+//
+// Created by Daniil Yacine on Jan 14, 2017
+//
+//          (more trumpets)
+//
+
+func nChar (str: String, indx: Int) -> String {
+    if str.isEmpty {
+        print("nChar error: empty string submitted")
+        return "nChar bad input"
+    } else if str.characters.count < indx {
+        print("nChar error: target index (\(indx)) exceeds the submitted string's total number of characters (\(str.characters.count))")
+        return "nChar bad input"
+    } else {
+        let zeroIndex = str.startIndex
+        let targetChar = str[str.index(zeroIndex, offsetBy: indx)]
+        return "\(targetChar)"
+    }
+}
+
+// Example of calling the nChar and using the produced result straight away:
+let itSnow = nChar(str: "Swift", indx: 0) + "now"
+print(itSnow) // This will print out 'Snow'. Well, not literally, but it will.
