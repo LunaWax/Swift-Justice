cdab53497b9e9a30e28fdb8a55ad419da71468bd
LunaWax <lunaisacoolguy@gmail.com>
2017-01-14 21:01:49 +0300

    update to 1.1
    - Conditional refactoring
    - Safety improvement (soft stop and alert for negative indices)

--- nChar.swift
+++ nChar.swift
@@ -1,5 +1,5 @@
 //
-// Swift Justice: nChar function 1.0.1
+// Swift Justice: nChar function 1.1.0
 //
 // The ingenious nChar function is there for you when you need an easy way to get any string's substring based
 // on its index, as if you were dealing with an array (similar to how it works in JavaScript).
@@ -7,7 +7,7 @@
 // **User Manual** :
 //  1. Call the nChar function with two parameters:
 //      str: any string (or constant/variable with a String-type value) you like
-//      indx: the target index (we like to count from 0, i.e. 'S' in 'Swift' has the index of 0)
+//      indx: the target index as a positive integer (we like to count from 0, i.e. 'S' in 'Swift' has the index of 0)
 //  2. Let the magic begin
 //  3. Receive the character you were looking for (!!!IMPORTANT!!) as a 'String', not 'Character'
 //     This means you can use it for your string-combining shady business straight away!
@@ -26,14 +26,22 @@ func nChar (str: String, indx: Int) -> String {
     if str.isEmpty {
         print("nChar error: empty string submitted")
         return "nChar bad input"
-    } else if str.characters.count-1 < indx {
-        print("nChar error: target index (\(indx)) exceeds the submitted string's final index (\(str.characters.count-1))")
-        return "nChar bad input"
-    } else {
-        let zeroIndex = str.startIndex
-        let targetChar = str[str.index(zeroIndex, offsetBy: indx)]
-        return "\(targetChar)"
     }
+    
+    if str.characters.count-1 < indx {
+        print("nChar error: target index (\(indx)) exceeds the '\(str)' string's final index (\(str.characters.count-1))")
+        return "nChar bad input"
+    }
+    
+    if indx < 0 {
+        print("nChar error: 'indx' must be a positive integer")
+        return "nChar bad input"
+    }
+    
+    let zeroIndex = str.startIndex
+    let targetChar = str[str.index(zeroIndex, offsetBy: indx)]
+    return "\(targetChar)"
+    
 }
 
 // Example of calling the nChar and using the produced result straight away:
