19cf7e17e856cabce8ec760c56d7dfd7a37d85f7
LunaWax <lunaisacoolguy@gmail.com>
2017-01-27 02:30:41 +0300

    major functionality update
    this is a sloppy solution, but isHaiku is now so much better at telling
    the number of vowel sounds.
    
    This update puts isHaiku high above and way ahead of its tasteless
    JavaScript copy.

--- isHaiku.swift
+++ isHaiku.swift
@@ -18,20 +18,47 @@ func nChar (_ str: String?, _ indx: Int?) -> String? {
     return nil
 }
 
+// isVow
+
+func isVow(_ str: String?) -> Bool? {
+    if var inpString = str, !inpString.isEmpty {
+        let vowels = ["а", "о", "э", "и", "у", "ы", "е", "ё", "ю", "я"]
+        inpString = inpString.lowercased()
+        if vowels.contains(inpString) {
+            return true
+        }
+        return false
+    }
+    return nil
+}
+
+// inArow
+func inArow(_ str: String) -> Int {
+    let clet = str.lowercased()
+    var counter = 0
+    for chars in 0...clet.characters.count - 2 {
+        if nChar(clet, chars)! == nChar(clet, chars + 1)! && isVow(nChar(clet, chars)!)! {
+            counter += 1
+        }
+    }
+    return counter
+}
+
 // vowelCount
 
 func vowelCount(_ str: String?) -> Int? {
     let vowels = ["а", "о", "э", "и", "у", "ы", "е", "ё", "ю", "я"]
-    if let inpString = str, !inpString.isEmpty {
+    if var inpString = str, !inpString.isEmpty {
+        inpString = inpString.lowercased()
         var vowelCounter: Int = 0
         for index in 0...inpString.characters.count - 1 {
-            //            print(nChar(inpString, index)!)
+            //                        print(nChar(inpString, index)!)
             if vowels.contains(nChar(inpString, index)!) {
-                //                print("Это гласная")
+                //                                print("нашел гласную \(nChar(inpString, index)!)")
                 vowelCounter += 1
             }
         }
-        return vowelCounter
+        return vowelCounter - inArow(inpString)
     }
     return nil
 }
